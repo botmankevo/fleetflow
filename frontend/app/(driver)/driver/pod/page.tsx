51a175272bfc44f0a7cac75e930645a39e141ab5
botmankevo <76708880+botmankevo@users.noreply.github.com>
2026-01-27 16:49:35 +0000

    fix: import useEffect in driver pod page

--- frontend/app/(driver)/driver/pod/page.tsx
+++ frontend/app/(driver)/driver/pod/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import UploadField from "../../../../components/UploadField";
 import SignaturePad from "../../../../components/SignaturePad";
